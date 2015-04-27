1ebbb48e43d9c041cf883e72ae2be7e240c7df85
Harrison Goldstein <hgoldstein95@gmail.com>
2015-04-27 00:45:50 -0400

    Made meaningless changes while Chris found out that console.log is evil.

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,3 +1,7 @@
+Meteor.startup(function() {
+	Hooks.init();
+});
+
 Meteor.subscribe("rooms");
 Meteor.subscribe("messages");
 Meteor.subscribe('cards');
@@ -6,7 +10,12 @@ Meteor.subscribe('users');
 Session.setDefault('alignment-filter', "all");
 Session.setDefault('current-page', null);
 Session.setDefault('removing', false);
+Session.setDefault('setup', {});
 
 Accounts.ui.config({
 	passwordSignupFields: "USERNAME_ONLY"
 });
+
+Hooks.onLoggedOut = function(userId) {
+	Router.go('/');
+};
