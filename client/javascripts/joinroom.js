7de54e4239d5c0cd2061383a704bd4e4b67fe4c4
Harrison Goldstein <hgoldstein95@gmail.com>
2015-04-28 16:27:26 -0400

    Prettified rooms page.

--- client/javascripts/joinroom.js
+++ client/javascripts/joinroom.js
@@ -1,5 +1,23 @@
 Template.joinroom.helpers({
 	'allRooms': function() {
 		return MafiaRooms.find().fetch();
+	},
+	'getmodname': function(room) {
+		return Meteor.users.findOne({_id: room.mod}).username;
+	}
+})
+
+Template.joinroom.events ({
+	'click a': function(e) {
+		var moderatorId = Meteor.users.findOne({username: $(e.target).data("value")})._id;
+		var idvalue = Meteor.userId();
+		var newPlayers = MafiaRooms.findOne({mod: moderatorId}).players;
+		if(!(idvalue in newPlayers)){
+			newPlayers[idvalue]=null;
+		}
+		MafiaRooms.update({_id: MafiaRooms.findOne({mod: moderatorId})._id} ,{
+			$set: {players: newPlayers}
+		});
+		Session.set("myModId",moderatorId);
 	}
 })
